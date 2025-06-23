f85f8593d27dce9b0a7f7ead4963f5e7cc624977
Petr Hejkal <petr.hejkal@finshape.com>
2025-06-23 21:10:52 +0200

    Detail completed

--- scripts/game-loader.js
+++ scripts/game-loader.js
@@ -1,37 +1,112 @@
 
 async function loadGame() {
-    const id = window.location.hash.substring(1)
+    const gameId = window.location.hash.substring(1)
 
-    if (!id) {
+    if (!gameId) {
         document.body.innerHTML = "<p>No game ID provided.</p>"
         return
     }
 
     try {
-        const res = await fetch(`games/${id}/data.json`)
+        const res = await fetch(`games/${gameId}/data.json`)
         if (!res.ok) throw new Error("Game not found")
         const game = await res.json()
 
-        document.getElementById("hero").style.backgroundImage = `url('games/${id}/title.jpg')`
-    //    document.getElementById("game-title-hero").textContent = game.name
-    //    document.getElementById("game-title-hero").style.color = game.color
-        document.getElementById("sticky-title").style.background = game.color
-        document.getElementById("game-title").textContent = game.name
-        
-        document.getElementById("players-value").textContent = game.players
-        document.getElementById("playtime-value").textContent = game.playtime
-        document.getElementById("weight-value").textContent = game.weight
-        
+        id("hero").style.backgroundImage = `url('games/${gameId}/title.jpg')`
+        id("sticky-title").style.background = game.color
+        id("game-title").textContent = game.name
+        id("game-description").innerHTML = marked.parse(game.description)
+        id("start-play-a").href = `bgstats://app.bgstatsapp.com/addPlay.html?gameId=${game.bgg}`
 
-        // document.getElementById("game-players").textContent = game.players
-        // document.getElementById("game-playtime").textContent = game.playtime
-        // document.getElementById("game-age").textContent = game.age
-        // document.getElementById("game-image").src = `images/${game.image}`
-        // document.getElementById("game-image").alt = game.name
+        fillPlayers(game)
+        fillPlaytime(game)
+        fillWeight(game)
+        fillLinks(game)
     } catch (err) {
-        // document.body.innerHTML = "<p>Error loading game data.</p>"
+        document.body.innerHTML = "<p>Error loading game data.</p>"
         console.error(err)
     }
+}
+
+function fillPlayers(game) {
+    const count = game.players
+    const container = id("players")
+
+    id("players-value").textContent = count
+    switch (count) {
+        case "1-3":
+        case "2-3":
+        case "1-4":
+        case "2-4":
+            container.classList.add("bg-success")
+            container.classList.add("border-success-subtle")
+            break
+        case "1-5":
+        case "2-5":
+            container.classList.add("bg-warning")
+            container.classList.add("border-warning-subtle")
+            break
+        default:
+            container.classList.add("bg-danger")
+            container.classList.add("border-danger-subtle")
+    }
+}
+
+function fillPlaytime(game) {
+    const time = game.playtime
+    const topTimeMatch = time.match(/-(\d+)$/)
+    const topTime = topTimeMatch ? Number.parseInt(topTimeMatch[1]) : -1
+    const container = id("playtime")
+
+    id("playtime-value").textContent = time
+    if (topTime <= 30) {
+        container.classList.add("bg-success")
+        container.classList.add("border-success-subtle")
+    } else if (topTime <= 60) {
+        container.classList.add("bg-warning")
+        container.classList.add("border-warning-subtle")
+    } else {
+        container.classList.add("bg-danger")
+        container.classList.add("border-danger-subtle")
+    }
+}
+
+function fillWeight(game) {
+    const weight = game.weight
+    const container = id("weight")
+
+    id("weight-value").textContent = weight
+    if (weight <= 1.4) {
+        container.classList.add("bg-success")
+        container.classList.add("border-success-subtle")
+    } else if (weight < 2) {
+        container.classList.add("bg-warning")
+        container.classList.add("border-warning-subtle")
+    } else {
+        container.classList.add("bg-danger")
+        container.classList.add("border-danger-subtle")
+    }
+}
+
+function fillLinks(game) {
+    let links = []
+    const linksHtml = links
+        .map(l => `<li class="list-inline-item"><img src="${l.i}"/> <a href="${l.u}" target="_blank">${l.t}</a></li>`)
+        .join("")
+
+    id("game-links").innerHTML = `<ul class="list-unstyled">
+        <li class="list-inline-item"><img src="https://cf.geekdo-static.com/icons/favicon2.ico" class="me-1"/><a href="https://boardgamegeek.com/boardgame/${game.bgg}" target="_blank">BGG</a></li>
+        <li class="list-inline-item ms-2"><img src="https://www.zatrolene-hry.cz/graphics/favicon.cz.svg" class="me-1"/><a href="https://www.zatrolene-hry.cz/spolecenska-hra/${game.zh}" target="_blank">Zatrolené hry</a></li>
+        ${linksHtml}
+    </ul>`
+}
+
+function id(elementId) {
+    return document.getElementById(elementId)
+}
+
+function startPlay(game) {
+alert(game.bgg)
 }
 
 loadGame()
